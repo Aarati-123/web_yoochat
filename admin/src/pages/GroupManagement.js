f66b5bf67c91b4e032dad2702355b24880e0e531
avipsahamo <avipsaofficial@gmail.com>
2025-11-16 07:24:11 +0545

    v1 admin

---
+++ admin/src/pages/GroupManagement.js
@@ -0,0 +1,14 @@
+import Navbar from "../components/Navbar";
+
+const GroupManagement = () => {
+  return (
+    <div>
+      <Navbar />
+      <div style={{ padding: "20px" }}>
+        <h1>GroupManagement</h1>
+      </div>
+    </div>
+  );
+};
+
+export default GroupManagement;
